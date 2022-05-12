b3575eaf6b3d233ed153454547d4e51cf7936e4f
mrgao125 <312595543@qq.com>
2022-05-12 12:54:33 +0800

    引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,20 @@
     <!-- 路由出口 -->
     <router-view/>
     <h1>黑马头条</h1>
+    <div>
+      <i class="iconfont icon-sousuo"></i>
+    </div>
+    <p>vant组件测试</p>
+    <van-button type="primary">主要按钮</van-button>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
+
   </div>
 </template>
 <script>
+
   export default {
     name:'App'
   }
--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import router from './router'
 // 加载全局样式
 import './styles/index.less'
 
+// 按需引入Vant 
+import { Button, Cell, CellGroup } from 'vant'
+Vue.use(Button)
+Vue.use(Cell)
+Vue.use(CellGroup)
+
+
 Vue.config.productionTip = false
 
 new Vue({
