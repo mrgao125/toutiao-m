47e49e5b48dbd1b3487cf160028fb42b1190cdd8
mrgao125 <312595543@qq.com>
2022-05-12 11:15:00 +0800

    初始化项目文件

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,1 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-
-Vue.use(Vuex)
-
-export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+// vuex入口文件
